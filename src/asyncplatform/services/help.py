f7f3b9c1fa5eff35e856560ae706f372d5c31016
Jenn Lu <jenn.lu@itential.com>
2026-02-04 10:15:30 -0500

    fix: add type annotation to help service url parameter

--- src/asyncplatform/services/help.py
+++ src/asyncplatform/services/help.py
@@ -4,8 +4,6 @@
 
 from __future__ import annotations
 
-import asyncio
-
 from typing import Any
 
 from asyncplatform import logging
@@ -16,7 +14,7 @@ class Service(ServiceBase):
     name: str = "help"
 
     @logging.trace
-    async def get_openapi(self, url=None) -> dict[str, Any]:
+    async def get_openapi(self, url: str | None = None) -> dict[str, Any]:
         """ """
         res = await self.get("/help/openapi", params={"url": (url or "/")})
         return res.json()
